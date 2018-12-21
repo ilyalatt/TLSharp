2a8e79f8fdeb08d8520e5929c177641e767b7ec8
ilya latushko <ilyalatt@gmail.com>
2018-12-21 21:24:39 +0300

    fix TLContext

--- TeleSharp.TL/TLContext.cs
+++ TeleSharp.TL/TLContext.cs
@@ -17,7 +17,6 @@ namespace TeleSharp.TL
                      where t.IsSubclassOf(typeof(TLObject))
                      where t.GetCustomAttribute(typeof(TLObjectAttribute)) != null
                      select t).ToDictionary(x => ((TLObjectAttribute)x.GetCustomAttribute(typeof(TLObjectAttribute))).Constructor, x => x);
-            Types.Add(481674261, typeof(TLVector<>));
         }
 
         public static Type GetType(int constructor)
